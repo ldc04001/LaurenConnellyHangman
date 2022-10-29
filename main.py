c03abb41a415f47300e76c055d10b8bf5f00b299
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-29 17:33:41 +0000

    all comments and title

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 # Create fun title
+print(r"""
+ __  __     ______     __   __     ______     __    __     ______     __   __    
+/\ \_\ \   /\  __ \   /\ "-.\ \   /\  ___\   /\ "-./  \   /\  __ \   /\ "-.\ \   
+\ \  __ \  \ \  __ \  \ \ \-.  \  \ \ \__ \  \ \ \-./\ \  \ \  __ \  \ \ \-.  \  
+ \ \_\ \_\  \ \_\ \_\  \ \_\\"\_\  \ \_____\  \ \_\ \ \_\  \ \_\ \_\  \ \_\\"\_\ 
+  \/_/\/_/   \/_/\/_/   \/_/ \/_/   \/_____/   \/_/  \/_/   \/_/\/_/   \/_/ \/_/ 
+                                                                                 
+""")
+print()
 
 # Give the user the game instructions
-
-# Create the variable for the secret word and blank variable for the user's word
-secretWord = "AMPLIFIER"
-userGuesses = []
-count = 0
+print("Welcome to the classic word game, Hangman!")
 
 
 # Create the function to get the user to enter a letter
 def getLetter():
-  userLetter = ""
   
+  userLetter = ""
+
+  # Force the user to enter only 1 letter
   while len(userLetter) != 1:
     
     userLetter = input("Enter a letter: ")
-    
+
+  # make the letter uppercase, if it is not
   return userLetter.upper()
   
 
 # Create the function to display the secret word
 def displaySecret(secret, guesses):
   
-# Go through each letter in the secret word, and determine HOW we display it
+# For each letter in the secretWord
   for letter in secret:
-    
+
+   # if that letter is also one of their guesses 
    if letter in guesses:
-           
+
+    # Show them the letter in the correct place
     # If this letter (from the secret word) has been guessed, display the letter
     print(letter, end ="")
     
      
-    # Otherwise, display an underscore ( _ )
+    # if the letter is not one of their guesses, then show a blank as a placeholder
    else: 
      print("_ ", end ="")
 
@@ -40,44 +50,73 @@ def displaySecret(secret, guesses):
 # Create a function to tell if the user has won or not
 def checkGuess(answer, guesses):
 
+  # set the default to True
   win = True
   
-  # Go through each letter in the secret word
+  # for each letter in the secret word
   for letter in answer:
-    
+
+    # If every letter in the secret word doesn't appear in the list of guesses
     if letter not in guesses :
-      
+
+      # the person did not win
       win = False
       
   return win
 
   
-# Design a loop for the game
+# Design a loop for the game:
+  
+# Create the variable for the secret word and blank list to hold all the user's guesses
+secretWord = "AMPLIFIER"
+userGuesses = []
 
+# set the user's guess count, or 'strikes' to zero
+count = 0
+
+# while the strikes are less than 6, and the user has not guessed all the letters in the word...
 while count < 6 and checkGuess(secretWord, userGuesses) != True:
+  
+  # Ask the user to enter a single letter
   userLetter = getLetter()
   print()
+  
+  # Add that letter to the userGuesses list
   userGuesses.append(userLetter) 
+
+  # If they guessed all the letters in the secret word
   if checkGuess(secretWord, userGuesses) == True:
-    print("you win")
+
+    # tell them they won
+    print("You win")
+    
+  # if their letter guess is in the secret word 
   if userLetter in secretWord:
-    print("good guess")
+    
+    # tell them good guess
+    print("Good guess!")
+    
+    # then show them where their letter is in the secret word
     displaySecret(secretWord, userGuesses)
     print()
-    
+  
+    # if their letter in not in the secret word
   else: 
-    
+
+    # Add a strike
     count += 1
+
+    # Tell them their letter is not in the secret word
     print(f"{userLetter} is not in the word")
-    print(f"you guessed wrong, you have {count} out of 6 strikes")
+
+    # show them the number of strikes they have
+    print(f"You guessed wrong, you have {count} out of 6 strikes")
+
+    # when they guess wrong 6 times
     if count == 6:
-      print("you lose")
+
+      # tell them they lost
+      print("Sorry, you lose!")
     print()
 
-    
   
-  
-  
-  
-  # Create a condition that stops the loop when the user has won or they've guessed 6 letters wrong
-
